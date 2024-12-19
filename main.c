feede500a4c306f48f97039df8dcaaa414009445
kazancevss <kazancevss@mail.ru>
2024-12-19 11:20:16 +0500

    first commit

---
+++ main.c
@@ -0,0 +1,28 @@
+/* 
+    Охранная система "ПериметрC" для защиты частного дома от незаконнного проникновения.
+    Для отображения информации исльзован LCD дисплей 20/4 от компании МЭЛТ
+    Для регистрации открывания двери использованы механические замыкатели (герконы)
+    Для регистрации перемещения использованы датчики движения 
+    ПО польностью переписано на языке C, исходны проект был на ASM
+    Copyright: Казанцев Сергей Сергеевич (kazancevss) 2025
+
+    The "PerimeterC" security system is designed to protect a private home from unauthorized access.
+    An LCD display 20x4 from MELT is used for information display.
+    Mechanical switches (reed switches) are employed for door opening detection.
+    Motion sensors are used for movement detection.
+    The software was completely rewritten in the C language, as the original project was in ASM.
+    Copyright: Sergey Sergeyevich Kazantsev (kazancevss), 2025.
+*/
+
+#define F_CPU 16000000UL
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+
+int main (void)
+{
+
+
+
+}
